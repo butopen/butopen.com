e3fe581a7c4a1b378c81c091da5235a2addf9253
salvatoreromeo <info@salvatoreromeo.com>
2022-01-19 14:53:25 +0100

    add loading and tabs components

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,5 +1,13 @@
 module.exports = {
     author: 'Salvatore A. Romeo',
     copyrightYear: (new Date()).getFullYear(),
-    host: "https://www.butopen.com"
+    host: "https://www.butopen.com",
+    posts: [{
+     url: "https://www.ssonext.com/blog/designing-an-open-source-single-sign-on-service-ssonext/",
+     data: {
+         title: "How to design a Single Sign-On Open Source Service - Starting from People",
+         description: "We will see the first step taken when designing a new service for developers. We will think about how developers will use it, what's the fastest way on the front-end, what the back-end will need",
+         image: "https://www.ssonext.com/blog/designing-an-open-source-single-sign-on-service-ssonext/shot1.jpg"
+     }   
+    }]
 }

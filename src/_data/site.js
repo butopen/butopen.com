2cef297b65d0b03341f5970804696df9acab3119
salvatoreromeo <info@salvatoreromeo.com>
2022-02-21 09:47:37 +0100

    add tailwind redevtools post

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,11 +3,20 @@ module.exports = {
     copyrightYear: (new Date()).getFullYear(),
     host: "https://www.butopen.com",
     posts: [{
-     url: "https://www.ssonext.com/blog/designing-an-open-source-single-sign-on-service-ssonext/",
-     data: {
-         title: "How to design a Single Sign-On Open Source Service - Starting from People",
-         description: "We will see the first step taken when designing a new service for developers. We will think about how developers will use it, what's the fastest way on the front-end, what the back-end will need",
-         image: "https://www.ssonext.com/blog/designing-an-open-source-single-sign-on-service-ssonext/shot1.jpg"
-     }   
-    }]
+        url: "https://www.ssonext.com/blog/designing-an-open-source-single-sign-on-service-ssonext/",
+        data: {
+            title: "How to design a Single Sign-On Open Source Service - Starting from People",
+            description: "We will see the first step taken when designing a new service for developers. We will think about how developers will use it, what's the fastest way on the front-end, what the back-end will need",
+            image: "https://www.ssonext.com/blog/designing-an-open-source-single-sign-on-service-ssonext/shot1.jpg"
+        }
+    },
+        {
+            url: "https://www.redevtools.com/blog/tailwind-devtools-inspector-how-to-quickly-edit-tailwind-classes-and-apply-tailwind-templates/",
+            data: {
+                title: "How to inspect a tailwind element and preview class changes live using the devtools",
+                description: "Tailwind Devtools Inspector - How to quickly edit tailwind classes and apply tailwind templates - no install - already in your console",
+                image: "https://www.redevtools.com/blog/tailwind-devtools-inspector-how-to-quickly-edit-tailwind-classes-and-apply-tailwind-templates/tailwind-devtool.gif"
+            }
+        }
+    ]
 }
